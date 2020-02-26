46677228c01113a44bfa760998891756607eb8d1
Raymond Pulver IV <ray@0confirmation.com>
2020-02-26 17:40:09 -0500

    update

--- packages/0confirmation-homepage/src/App.js
+++ packages/0confirmation-homepage/src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainInfo from './0cf_lp_fn-01.svg';
+import EmailInfo from './0cf_lp_fn-02.svg';
+import TelegramInfo from './0cf_lp_fn-03.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div><img src={MainInfo} className="App-logo" alt="logo" /></div>
+        <div><a href="mailto:info@0confirmation.com"><img src={EmailInfo} className="App-logo" alt="logo" /></a></div>
+        <div><img src={TelegramInfo} className="App-logo" alt="logo" /></div>
       </header>
     </div>
   );
