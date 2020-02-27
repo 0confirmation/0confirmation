74f46d8be71d0ed7eac2be0ef264902d955832f3
Raymond Pulver IV <ray@0confirmation.com>
2020-02-26 20:08:27 -0500

    update

--- packages/0confirmation-homepage/src/App.js
+++ packages/0confirmation-homepage/src/App.js
@@ -7,10 +7,11 @@ import TelegramInfo from './0cf_lp_fn-03.svg';
 function App() {
   return (
     <div className="App">
+      <title>0confirmation - decentralized signaling network for instant liquidity access</title>
       <header className="App-header">
         <div><img src={MainInfo} className="App-logo" alt="logo" /></div>
         <div><a href="mailto:info@0confirmation.com"><img src={EmailInfo} className="App-logo" alt="logo" /></a></div>
-        <div><img src={TelegramInfo} className="App-logo" alt="logo" /></div>
+        <div><a href="t.me/zeroconfirmation"><img src={TelegramInfo} className="App-logo" alt="logo" /></a></div>
       </header>
     </div>
   );
