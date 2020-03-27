982ba071f00f617ee60f0dbac969ab86e5917528
Adetayo Timilehin <timilehin1999@gmail.com>
2020-03-27 01:38:11 +0000

    included qr code

--- packages/0confirmation-homepage/src/App.js
+++ packages/0confirmation-homepage/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MainInfo from './0cf_lp_fn-01.svg';
 import EmailInfo from './0cf_lp_fn-02.svg';
 import TelegramInfo from './0cf_lp_fn-03.svg';
+import Qrcode from './qrcode.jpeg';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <header className="App-header">
         <div style={ { height: '100%' } }>
           <div><img src={MainInfo} className="App-logo" alt="logo" /></div>
+          <div><img src={Qrcode} className="App-logo" alt="qrcode" /></div>
           <div><a href="mailto:info@0confirmation.com"><img src={EmailInfo} className="App-logo" alt="logo" /></a></div>
           <div><a href="https://t.me/zeroconfirmation"><img src={TelegramInfo} className="App-logo" alt="logo" /></a></div>
         </div>
