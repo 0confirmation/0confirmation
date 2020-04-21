a2c58ea52405ecff36cfff4e1c88b4912d701cad
sidechainman <dudemakinchains@gmail.com>
2020-04-21 05:26:30 +0000

    update

--- packages/0confirmation-registry/up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-source ./lib/functions.sh
-source ./lib/digitalocean.sh
-
-create_machine npm-registry
-docker-machine scp ./nginx.conf npm-registry:nginx.conf
-target npm-registry
