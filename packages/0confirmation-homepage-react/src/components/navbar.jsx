3813aff4a57ffe447c5203261843874a5c1d686c
Adetayo Timilehin <timilehin1999@gmail.com>
2020-05-04 06:50:01 +0000

    new

--- packages/0confirmation-homepage-react/src/components/navbar.jsx
+++ packages/0confirmation-homepage-react/src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, NavLink, NavItem, NavbarBrand, NavbarToggler } from "reactstrap";
+import {Link} from "react-router-dom";
 import "../App.css";
 import { IoIosMenu } from "react-icons/io";
 import Drawer from '@material-ui/core/Drawer';
@@ -9,6 +10,7 @@ export default class Navigation extends React.Component {
         this.state = {
             isopen: false,
             isScroll: false,
+            position: null,
         }
 
         // this.initchecking = this.checking.bind(this);
@@ -23,8 +25,12 @@ export default class Navigation extends React.Component {
         }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        var baseurl = window.location.pathname.split("/")[1];
+        var id = window.location.pathname.split("/")[2];
+        if (baseurl === "trade") {
+            await this.setState({ position: id });
+        }
         // this.initchecking()
         window.addEventListener('scroll', this.checking)
 
@@ -38,7 +44,7 @@ export default class Navigation extends React.Component {
             <>
                 <div>
                     {(this.props.ismobile)?
-                        <Navbar fixed="top" className={(this.state.isScroll) ? "px-5 py-3 shadow-lg bgs" : "px-5 py-3"}>
+                        <Navbar fixed="top" className={(this.state.isScroll) ? "px-5 py-3 shadow-sm bgs" : "px-5 py-3"}>
                             <NavbarBrand href="/"><img src={require("../images/0cf.svg")} alt="0cf" /></NavbarBrand>
                             <NavLink className="ml-auto" onClick={
                                 async e => {
@@ -54,14 +60,20 @@ export default class Navigation extends React.Component {
                                     onClick={async () => { await this.setState({ isopen: false }) }}
                                     onKeyDown={async () => { await this.setState({ isopen: false }) }}>
                                     <Nav vertical>
-                                        <NavItem>
-                                            <NavLink style={{ color: "#ffffff" }} href="/#">Docs</NavLink>
+                                        <NavItem className="p-2" style={{ backgroundColor: (window.location.pathname.split("/")[2] === "doc") ? "#1F2820" : "" }}>
+                                            <Link to="/" style={{outline:"none", textDecoration:"none", color: "#ffffff"}} href="/#" className="my-2"
+              
+                                            >Docs</Link>
                                         </NavItem>
-                                        <NavItem>
-                                            <NavLink style={{ color: "#ffffff" }} href="/#">Earn</NavLink>
+                                        <NavItem className="p-2" style={{ backgroundColor: (window.location.pathname.split("/")[2] === "earn") ? "#1F2820" : ""}}>
+                                            <Link to="/trade/earn" style={{outline:"none", textDecoration:"none", color: "#ffffff" }} href="/#" className="my-2"
+              
+                                            >Earn</Link>
                                         </NavItem>
-                                        <NavItem>
-                                            <NavLink style={{ color: "#ffffff" }} href="/#">Swap</NavLink>
+                                        <NavItem className="p-2" style={{ backgroundColor: (window.location.pathname.split("/")[2] === "swap") ? "#1F2820" : "" }}>
+                                            <Link to="/trade/swap" style={{outline:"none", textDecoration:"none", color: "#ffffff" }} href="/#" className="my-2"
+                                   
+                                            >Swap</Link>
                                         </NavItem>
                                     </Nav>
                                 </div>
@@ -69,18 +81,24 @@ export default class Navigation extends React.Component {
                             
                         </Navbar>
                     :    
-                    <Navbar fixed="top" className={(this.state.isScroll) ? "px-5 py-3 shadow-lg bgs" : "px-5 py-3"}>
+                    <Navbar fixed="top" className={(this.state.isScroll) ? "px-5 py-3 shadow-sm bgs" : "px-5 py-3"}>
                         <NavbarBrand href="/"><img src={require("../images/0cf.svg")} alt="0cf"/></NavbarBrand>
                         <NavbarToggler onClick={async()=>await this.setState({isopen:!this.state.isopen})} />
                         <Nav className="ml-auto">
                             <NavItem>
-                                <NavLink style={{ color: "#ffffff" }} href="/#">Docs</NavLink>
+                                <Link to="/" style={{width:"115px", height:"50px", padding:"10px",outline:"none", textDecoration:"none", color: "#ffffff", backgroundColor:(window.location.pathname.split("/")[2] === "doc")?"#1F2820":"", borderRadius:(window.location.pathname.split("/")[2] === "doc")?"10px":"" }} className="mx-1" href="/#"
+                                  
+                                >Docs</Link>
                             </NavItem>
                             <NavItem>
-                                <NavLink style={{ color: "#ffffff" }} href="/#">Earn</NavLink>
+                                    <Link to="/trade/earn" style={{width:"115px", height:"50px", padding:"10px",outline:"none", textDecoration:"none", color: "#ffffff", backgroundColor:(window.location.pathname.split("/")[2] === "earn")?"#1F2820":"", borderRadius:(window.location.pathname.split("/")[2] === "earn")?"10px":"" }} className="mx-1" href="/#"
+                            
+                                >Earn</Link>
                             </NavItem>
                             <NavItem>
-                                <NavLink style={{ color: "#ffffff" }} href="/#">Swap</NavLink>
+                                    <Link to="/trade/swap" style={{width:"115px", height:"50px", padding:"10px",outline:"none", textDecoration:"none", color: "#ffffff", backgroundColor:(window.location.pathname.split("/")[2] === "swap")?"#1F2820":"", borderRadius:(window.location.pathname.split("/")[2] === "swap")?"10px":"" }} className="mx-1" href="/#"
+ 
+                                >Swap</Link>
                             </NavItem>
                        </Nav>
                     </Navbar>}
