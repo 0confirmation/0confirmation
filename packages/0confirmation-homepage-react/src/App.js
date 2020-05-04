8b5e24460acecec936f8afc6b5961dc7efa97e18
Adetayo Timilehin <timilehin1999@gmail.com>
2020-05-04 06:43:31 +0000

    Update App.js

--- packages/0confirmation-homepage-react/src/App.js
+++ packages/0confirmation-homepage-react/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import Home from './components/home';
+import Trade from './components/trade';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import './fonts/fonts.scss';
 
 function App() {
   return (
+    
     <div className="App">
-      <Home/>
+      <Router>
+      <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/trade' component={Trade} />
+      </Switch>
+    </Router>
     </div>
   );
 }
