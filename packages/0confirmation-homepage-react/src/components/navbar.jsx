55d7c7b1a1cd0b69cd4821c3f83fbcc76f1620fb
Adetayo Timilehin <timilehin1999@gmail.com>
2020-05-05 21:19:40 +0000

    Update packages/0confirmation-homepage-react/src/components/navbar.jsx

--- packages/0confirmation-homepage-react/src/components/navbar.jsx
+++ packages/0confirmation-homepage-react/src/components/navbar.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Navbar, Nav, NavLink, NavItem, NavbarBrand, NavbarToggler } from "reactstrap";
 import {Link} from "react-router-dom";
@@ -61,17 +62,17 @@ export default class Navigation extends React.Component {
                                     onKeyDown={async () => { await this.setState({ isopen: false }) }}>
                                     <Nav vertical>
                                         <NavItem className="p-2" style={{ backgroundColor: (window.location.pathname.split("/")[2] === "doc") ? "#1F2820" : "" }}>
-                                            <Link to="/" style={{outline:"none", textDecoration:"none", color: "#ffffff"}} href="/#" className="my-2"
+                                            <Link to="/" style={{outline:"none", textDecoration:"none", color: "#ffffff"}} className="my-2"
               
                                             >Docs</Link>
                                         </NavItem>
                                         <NavItem className="p-2" style={{ backgroundColor: (window.location.pathname.split("/")[2] === "earn") ? "#1F2820" : ""}}>
-                                            <Link to="/trade/earn" style={{outline:"none", textDecoration:"none", color: "#ffffff" }} href="/#" className="my-2"
+                                            <Link to="/trade/earn" style={{outline:"none", textDecoration:"none", color: "#ffffff" }} className="my-2"
               
                                             >Earn</Link>
                                         </NavItem>
                                         <NavItem className="p-2" style={{ backgroundColor: (window.location.pathname.split("/")[2] === "swap") ? "#1F2820" : "" }}>
-                                            <Link to="/trade/swap" style={{outline:"none", textDecoration:"none", color: "#ffffff" }} href="/#" className="my-2"
+                                            <Link to="/trade/swap" style={{outline:"none", textDecoration:"none", color: "#ffffff" }} className="my-2"
                                    
                                             >Swap</Link>
                                         </NavItem>
@@ -86,17 +87,17 @@ export default class Navigation extends React.Component {
                         <NavbarToggler onClick={async()=>await this.setState({isopen:!this.state.isopen})} />
                         <Nav className="ml-auto">
                             <NavItem>
-                                <Link to="/" style={{width:"115px", height:"50px", padding:"10px",outline:"none", textDecoration:"none", color: "#ffffff", backgroundColor:(window.location.pathname.split("/")[2] === "doc")?"#1F2820":"", borderRadius:(window.location.pathname.split("/")[2] === "doc")?"10px":"" }} className="mx-1" href="/#"
+                                <Link to="/" style={{width:"115px", height:"50px", padding:"10px",outline:"none", textDecoration:"none", color: "#ffffff", backgroundColor:(window.location.pathname.split("/")[2] === "doc")?"#1F2820":"", borderRadius:(window.location.pathname.split("/")[2] === "doc")?"10px":"" }} className="mx-1" 
                                   
                                 >Docs</Link>
                             </NavItem>
                             <NavItem>
-                                    <Link to="/trade/earn" style={{width:"115px", height:"50px", padding:"10px",outline:"none", textDecoration:"none", color: "#ffffff", backgroundColor:(window.location.pathname.split("/")[2] === "earn")?"#1F2820":"", borderRadius:(window.location.pathname.split("/")[2] === "earn")?"10px":"" }} className="mx-1" href="/#"
+                                    <Link to="/trade/earn" style={{width:"115px", height:"50px", padding:"10px",outline:"none", textDecoration:"none", color: "#ffffff", backgroundColor:(window.location.pathname.split("/")[2] === "earn")?"#1F2820":"", borderRadius:(window.location.pathname.split("/")[2] === "earn")?"10px":"" }} className="mx-1" 
                             
                                 >Earn</Link>
                             </NavItem>
                             <NavItem>
-                                    <Link to="/trade/swap" style={{width:"115px", height:"50px", padding:"10px",outline:"none", textDecoration:"none", color: "#ffffff", backgroundColor:(window.location.pathname.split("/")[2] === "swap")?"#1F2820":"", borderRadius:(window.location.pathname.split("/")[2] === "swap")?"10px":"" }} className="mx-1" href="/#"
+                                    <Link to="/trade/swap" style={{width:"115px", height:"50px", padding:"10px",outline:"none", textDecoration:"none", color: "#ffffff", backgroundColor:(window.location.pathname.split("/")[2] === "swap")?"#1F2820":"", borderRadius:(window.location.pathname.split("/")[2] === "swap")?"10px":"" }} className="mx-1" 
  
                                 >Swap</Link>
                             </NavItem>
