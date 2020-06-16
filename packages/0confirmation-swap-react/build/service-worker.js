c6daa0707b4dcf477c29ae6ce4855086d539b3b6
Raymond Pulver IV <ray@0confirmation.com>
2020-06-15 21:24:14 -0400

    update

--- packages/0confirmation-swap-react/build/service-worker.js
+++ packages/0confirmation-swap-react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d10c1549dc5c7eff8441ab5734fa642.js"
+  "/precache-manifest.2244c370821762c2c698c77b9b3255e9.js"
 );
 
 self.addEventListener('message', (event) => {
