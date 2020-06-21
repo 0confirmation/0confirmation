d6c64d6e35dab34c98d456902099786300d8c432
Raymond Pulver IV <raymond.pulver@my.uri.edu>
2020-06-21 20:03:18 +0000

    update

--- packages/0confirmation-swap-react/src/lib/bitcoin-helpers.test.js
+++ packages/0confirmation-swap-react/src/lib/bitcoin-helpers.test.js
@@ -1,8 +1,8 @@
 import * as blockchain from './bitcoin-helpers';
 
 test('should lookup blockcount', async () => {
-  console.log(await blockchain.getLatestBlock());
+//  console.log(await blockchain.getLatestBlock());
   console.log(await blockchain.getReceivedByAddress('3Ft1DpcKLVXzEa3yDks6yPo1chfHcaYDcH'));
-  console.log(await blockchain.getBlockCount('3Ft1DpcKLVXzEa3yDks6yPo1chfHcaYDcH'));
+ // console.log(await blockchain.getBlockCount('3Ft1DpcKLVXzEa3yDks6yPo1chfHcaYDcH'));
   console.log(await blockchain.getBlockCount('3Ft1DpcKLVXzEa3yDks6yPo1chfHcaYDcF'));
 });
