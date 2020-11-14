d75a0697a10c6733ab88718d535e107cc71bc936
ray <bandanation@gmail.com>
2020-11-13 22:43:29 -0500

    update ethers-base versions

--- packages/0confirmation-keeper/make-zero.js
+++ packages/0confirmation-keeper/make-zero.js
@@ -11,10 +11,22 @@ const Zero = require('@0confirmation/sdk');
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID || '2f1de898efb74331bf933d3ac469b98d';
 
 const signer = new ethers.Wallet(process.env.PRIVATE_KEY).connect(new ethers.providers.InfuraProvider(ETH_NETWORK, INFURA_PROJECT_ID));
+const gasnow = require('ethers-gasnow');
+const { RedispatchSigner } = require('ethers-redispatch-signer');
+signer.provider.getGasPrice = gasnow.createGetGasPrice('rapid');
 console.log(NETWORK);
 
 const makeZero = () => {
-  const zero = new Zero(signer, NETWORK);
+  const redispatchSigner = new RedispatchSigner(signer);
+  redispatchSigner.startWatching();
+  const nonces = {};
+  redispatchSigner.on('tx:dispatch', (tx) => {
+    if (nonces[Number(tx.nonce)]) {
+      console.logKeeper('tx redispatch: ' + tx.hash + '(' + ethers.utils.formatUnits(tx.gasPrice, 9) ' gwei)');
+    }
+    nonces[Number(tx.nonce)] = true;
+  });
+  const zero = new Zero(redispatchSigner, NETWORK);
   return zero;
 };
 
