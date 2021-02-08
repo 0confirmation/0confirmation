617a786236d6d7a9b426c310c7ede21bde8b1f01
Raymond Pulver IV <raymondpulver@my.uri.edu>
2021-02-08 23:41:25 +0000

    add svgs to url-loader rule

--- packages/0confirmation-homepage-react/config-overrides.js
+++ packages/0confirmation-homepage-react/config-overrides.js
@@ -4,6 +4,8 @@ module.exports = {
   webpack: (config, env) => {
     const rule = config.module.rules[1].oneOf.find((v) => v.test.toString().match('png'));
     rule.options.esModule = false;
+    rule.options.limit = 1e10;
+    rule.test.push(/\.svg$/i);
     return config;
   }
 };
